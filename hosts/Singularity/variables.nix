ec58a8d15a2de8ea520ef5820a4127814b8365e7
noelle-byte <finmaynard@proton.me>
2026-09-15 20:29:18 +0100

    desktop prep

--- hosts/Singularity/variables.nix
+++ hosts/Singularity/variables.nix
@@ -1,28 +1,28 @@
 {
-  username = "railgun"; # auto-set with install.sh, live-install.sh, and rebuild scripts.
+  username = "noelle"; # auto-set with install.sh, live-install.sh, and rebuild scripts.
 
   # Desktop Environment
   desktop = "hyprland"; # hyprland, i3, gnome, plasma6
 
   # Theme & Appearance
-  bar = "noctalia"; # waybar, noctalia, wayle
+  bar = "waybar"; # waybar, noctalia, wayle
   waybarTheme = "minimal"; # stylish, minimal
   sddmTheme = "astronaut"; # astronaut, black_hole, purple_leaves, jake_the_dog, hyprland_kath
-  defaultWallpaper = "evening-sky.webp"; # Change with SUPER + SHIFT + W (Hyprland)
+  defaultWallpaper = "duskwire.webp"; # Change with SUPER + SHIFT + W (Hyprland)
   hyprlockWallpaper = "kurzgesagt-galaxies.webp";
 
   # Default Applications
   terminal = "kitty"; # kitty, alacritty, wezterm
-  editor = "nixvim"; # nixvim, vscode, helix, doom-emacs, nvchad, neovim
-  browser = "zen-beta"; # zen-beta, firefox, floorp
-  fileManager = "yazi"; # yazi, lf, thunar
+  editor = "vscode"; # nixvim, vscode, helix, doom-emacs, nvchad, neovim
+  browser = "firefox"; # zen-beta, firefox, floorp
+  fileManager = "thunar"; # yazi, lf, thunar
   shell = "zsh"; # bash, zsh
   games = true; # Enable/Disable gaming module
 
   # Hardware
-  hostname = "Singularity";
+  hostname = "DesktopOfNightmares";
   videoDriver = "nvidia"; # nvidia, amdgpu, intel
-  nvidiaChannel = "legacy_580"; # stable, latest, beta, legacy_xxx
+  nvidiaChannel = "stable"; # stable, latest, beta, legacy_xxx
   bluetoothSupport = false; # Whether your motherboard supports bluetooth
   batterySupport = false; # Whether device has a battery (laptop)
 
